69e4aa9679a4eacad8dc62db68544828c0a2c022
osama-kh <75173358+osama-kh@users.noreply.github.com>
2022-11-13 19:39:40 +0200

    first project

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+//void main() {
+// runApp(MyApp());
+//}
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    //throw UnimplementedError();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('This is my defaulte text!'),
+      ),
+    );
+  }
+}
